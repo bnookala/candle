321c6a565c48e9b6a4b888f5f28ced29e2c83f44
Bhargav Nookala <nooknb@gmail.com>
2013-03-13 02:21:13 -0700

    adding utility wrappers

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,48 @@
+
+function getCurrentTab (callback) {
+    chrome.tabs.getCurrent(callback);
+};
+
+function getTab (tabId, callback) {
+    chrome.tabs.get(tabId, callback);
+};
+
+function createTab (newTabProperties, callback) {
+    chrome.tabs.get(newTabProperties, callback);
+};
+
+function copyTab (tabId, callback) {
+    chrome.tabs.duplicate(tabId, callback);
+};
+
+function findTabs (findTabProperties, callback) {
+    chrome.tabs.query(findTabProperties, callback);
+};
+
+function updateTab (tabId, propertiesToUpdate, callback) {
+    chrome.tabs.update(tabId, propertiesToUpdate, callback);
+};
+
+function moveTab (tabId, moveProperties, callback) {
+    chrome.tabs.move(tabId, moveProperties, callback);
+};
+
+function moveManyTabs (tabIds, moveProperties, callback) {
+    chrome.tabs.move(tabIds, moveProperties, callback);
+};
+
+function refreshTab (tabId, refreshProperties, callback) {
+    chrome.tabs.reload(tabId, refreshProperties, callbck);
+};
+
+function closeTab (tabId, callback) {
+    chrome.tabs.remove(tabId, callback);
+};
+
+function closeManyTabs (tabIds, callback) {
+    chrome.tabs.remove(tabIds, callback);
+};
+
 chrome.tabs.query({status:'complete'}, function (tabs) {
     console.log(tabs);
 });
