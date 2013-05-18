fdbdfe6276c991b73094930f0966cfc8a75388e4
Bhargav Nookala <nooknb@gmail.com>
2013-05-18 14:21:52 -0700

    Comitting on behalf of James Proud

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var allPagesContainer = $('.all-pages');
 
-	allPagesContainer.delegate('.client-action', 'click', function (event) {
+	allPagesContainer.delegate('.client-action.select', 'click', function (event) {
 		event.preventDefault();
 		var target = $(event.currentTarget).attr('href');
 
@@ -9,6 +9,19 @@ $(document).ready(function() {
 			// Need some kind of data binding. Timing isn't right here, and full page
 			// refreshes don't seem to show the correct state :/
 			window.location.reload();
+		});
+
+	});
+
+	allPagesContainer.delegate('.client-action.close', 'click', function (event) {
+		event.preventDefault();
+		var targetElement = $(event.currentTarget);
+		var targetHref = targetElement.attr('href');
+
+		$.get(targetHref, function () {
+			// Need some kind of data binding. Timing isn't right here, and full page
+			// refreshes don't seem to show the correct state :/
+			targetElement.parents('.single-tab').parent('li').hide();
 		});
 	});
 });
