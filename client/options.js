1febc920b94d83e3ff511505c57e6b95a2a3af68
Bhargav Nookala <nooknb@gmail.com>
2014-12-31 14:10:38 -0800

    Moving localstorage to chrome settings.

--- client/options.js
+++ client/options.js
@@ -1,16 +1,26 @@
-// Save this script as `options.js`
-
 var candleOptionsApp = angular.module('candleOptionsApp', []);
 
 candleOptionsApp.controller('OptionsController', function ($scope) {
-	$scope.serverAddress = window.localStorage['serverAddress'];
-	$scope.monitorName = window.localStorage['monitorName'];
+	$scope.init = function () {
+		var context = this;
+
+		chrome.storage.sync.get(['serverAddress', 'monitorName'], function (items) {
+			context.serverAddress = items['serverAddress'];
+			context.monitorName = items['monitorName'];
+		});
+	};
 
 	$scope.submit = function (monitorName, serverAddress) {
 		if (serverAddress) {
-			window.localStorage["serverAddress"] = serverAddress;
+			chrome.storage.sync.set({'serverAddress': serverAddress});
 		} else {
-			window.localStorage["serverAddress"] = "http://127.0.0.1:8090"
+			chrome.storage.sync.set({'serverAddress': "http://127.0.0.1:8090"});
+		}
+
+		if (monitorName) {
+			chrome.storage.sync.set({'monitorName': monitorName});
 		}
 	};
+
+	$scope.init();
 });
